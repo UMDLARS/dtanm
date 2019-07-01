9f5b1015d3cf7577d19ffaa950d5d13cc21d1a66
Chandler Swift <chandler@chandlerswift.com>
2019-07-01 12:08:41 -0500

    Hide sidebar toggle on desktop

--- scorer/scorer/ui.py
+++ scorer/scorer/ui.py
@@ -13,35 +13,19 @@ bp = Blueprint('ui', __name__)
 def connect_to_mongo():
     connect_mongo()
 
-
-@bp.route('/team/<team_name>')
-def team_update(team_name):
-    app.logger.debug(f"Code update for team: '{team_name}'")
-    team = get_team_manager().process_team(team_name)
-    if not team:  # If the team was invalid.
-        abort(400)
-
-    add_team(team.id)
-    return jsonify(id=team.id)
-
-
-@bp.route('/attack/<attack_name>')
-def new_attack(attack_name):
-    app.logger.debug(f"New Attack submitted: '{attack_name}'")
-    attack = get_attack_manager().process_attack(attack_name)
-    if not attack:  # If the attack was invalid.
-        abort(400)
-
-    add_attack(attack.id)
-    return jsonify(id=attack.id)
-
-
-@bp.route('/results')
-def get_results():
-    app.logger.debug(f'Got results request')
-    return Result.objects.to_json()
-
-
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@bp.route('/scoring')
+def show_scoreboard():
+    return render_template('scoring.html')
+
+@bp.route('/scoring/team/<int:team_id>')
+def show_team_score(team_id):
+    return render_template('team_score.html')
+
+@bp.route('/attacks')
+def attacks():
+    return render_template('attacks.html')
