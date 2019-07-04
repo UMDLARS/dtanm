d11ab5c46069acd6d2c37e5c0791fc22c79f27da
Chandler Swift <chandler@chandlerswift.com>
2019-07-04 13:43:12 -0500

    Update UI

--- scorer/scorer/ui.py
+++ scorer/scorer/ui.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, jsonify, render_template
+from flask import Blueprint, abort, jsonify, render_template, request, url_for
 from flask import current_app as app
 
 from scorer.db.conn import connect_mongo
@@ -13,19 +13,53 @@ bp = Blueprint('ui', __name__)
 def connect_to_mongo():
     connect_mongo()
 
+@bp.context_processor
+def utility_processor():
+    def create_menu_item(title: str, route_name: str):
+        """
+        Creates HTML for sidebar menu item, and highlights it if active
+
+        Parameters:
+            title (str): The displayed text of the menu item
+            route_name (str): the Python path to the route (e.g. 'ui.index')
+        """
+        selected_class = "active" if request.path == url_for(route_name) else "bg-light"
+        return f"<a href=\"{ url_for(route_name) }\" class=\"list-group-item list-group-item-action { selected_class }\">{title}</a>" 
+    return dict(create_menu_item=create_menu_item)
+
 @bp.route('/')
 def index():
     return render_template('index.html')
 
-
 @bp.route('/scoring')
-def show_scoreboard():
+def scoring():
     return render_template('scoring.html')
 
-@bp.route('/scoring/team/<int:team_id>')
+@bp.route('/rankings')
+def rankings():
+    return render_template('rankings.html')
+
+@bp.route('/challenge')
+def challenge():
+    return render_template('challenge.html')
+
+@bp.route('/scoring/teams/<int:team_id>')
 def show_team_score(team_id):
     return render_template('team_score.html')
 
-@bp.route('/attacks')
-def attacks():
-    return render_template('attacks.html')
+@bp.route('/submit_attack')
+def submit_attack():
+    return render_template('submit_attack.html')
+
+@bp.route('/submit_program')
+def submit_program():
+    return render_template('submit_program.html')
+
+@bp.route('/gold')
+def test_against_gold():
+    return render_template('test_against_gold.html')
+
+@bp.route('/stats')
+def stats():
+    return render_template('stats.html')
+
