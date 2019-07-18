4d8e1d3f9b261d5b1aec4b7a83fd627e793679dc
Chandler Swift <chandler@chandlerswift.com>
2019-07-18 10:25:41 -0500

    Add remaining UI pages

--- scorer/scorer/ui.py
+++ scorer/scorer/ui.py
@@ -14,12 +14,6 @@ ui_bp = Blueprint('ui', __name__)
 def connect_to_mongo():
     pass # connect_mongo()
 
-@ui_bp.before_request
-@login_required
-def before_request():
-    """ Protect all of the login-required endpoints. """
-    pass 
-
 @ui_bp.context_processor
 def utility_processor():
     def create_menu_item(title: str, route_name: str):
@@ -35,10 +29,12 @@ def utility_processor():
     return dict(create_menu_item=create_menu_item)
 
 @ui_bp.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
 @ui_bp.route('/scoring')
+@login_required
 def scoring():
     return render_template('scoring.html')
 
@@ -47,26 +43,39 @@ def rankings():
     return render_template('rankings.html')
 
 @ui_bp.route('/challenge')
+@login_required
 def challenge():
     return render_template('challenge.html')
 
 @ui_bp.route('/scoring/teams/<int:team_id>')
+@login_required
 def show_team_score(team_id):
     return render_template('team_score.html')
 
 @ui_bp.route('/submit_attack')
+@login_required
 def submit_attack():
     return render_template('submit_attack.html')
 
 @ui_bp.route('/submit_program')
+@login_required
 def submit_program():
     return render_template('submit_program.html')
 
 @ui_bp.route('/gold')
+@login_required
 def test_against_gold():
     return render_template('test_against_gold.html')
 
 @ui_bp.route('/stats')
 def stats():
-    return render_template('stats.html')
+    stats={
+        "Teams competing": "3",
+        "Attacks submitted": "42",
+        "Average score time": "3521ms",
+        "Attacks in scoring queue": "0",
+        "Scoring workers": "8",
+        "Idle scoring workers": "6",
+    }
+    return render_template('stats.html', stats=stats)
 
