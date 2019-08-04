a89bfb0f902fefa6047dd4b1a5277f43551785a6
Chandler Swift <chandler@chandlerswift.com>
2019-08-04 16:24:14 -0500

    Fix instructions page

--- web/web/blueprints/instructions/__init__.py
+++ web/web/blueprints/instructions/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, abort
 from werkzeug.utils import secure_filename
 import os
 
-instructions = Blueprint('instructions.index', __name__, template_folder='templates')
+instructions = Blueprint('instructions', __name__, template_folder='templates')
 
 @instructions.route('/', defaults={'page': 'index'})
 @instructions.route('/<page>')
@@ -10,12 +10,12 @@ def show(page):
     base_file=f'pack/instructions/{secure_filename(page)}'
     if os.path.isfile(base_file+'.html'):
         with open(base_file+'.html') as file:
-            return render_template('instructions.html', page=file.read(), format="html")
+            return render_template('instructions/index.html', page=file.read(), format="html")
     elif os.path.isfile(base_file+'.md'):
         with open(base_file+'.md') as file:
-            return render_template('instructions.html', page=file.read(), format="markdown")
+            return render_template('instructions/index.html', page=file.read(), format="markdown")
     elif os.path.isfile(base_file+'.txt'):
         with open(base_file+'.txt') as file:
-            return render_template('instructions.html', page=file.read(), format="text")
+            return render_template('instructions/index.html', page=file.read(), format="text")
     else:
         abort(404)
