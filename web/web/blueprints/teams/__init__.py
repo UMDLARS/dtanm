70b979faa3abac8a8faf1034750fc01cfbd962f8
Chandler Swift <chandler@chandlerswift.com>
2019-08-04 16:25:03 -0500

    Update teams display

--- web/web/blueprints/teams/__init__.py
+++ web/web/blueprints/teams/__init__.py
@@ -12,9 +12,9 @@ def index():
 @teams.route('/<int:team_id>')
 def show(team_id):
     team=Team.query.get_or_404(team_id)
-    return render_template('teams/show_team.html', team=team)
+    return render_template('teams/show.html', team=team)
 
 @teams.route('/me')
 @login_required
 def me():
-    return render_template('teams/show_team.html', team=current_user.team)
+    return render_template('teams/show.html', team=current_user.team)
