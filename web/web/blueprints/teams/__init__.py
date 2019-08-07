7231a225fbec21be9a5ae6f255f68bf3afcd3133
Chandler Swift <chandler@chandlerswift.com>
2019-08-07 12:51:27 -0500

    Display passing score of teams

--- web/web/blueprints/teams/__init__.py
+++ web/web/blueprints/teams/__init__.py
@@ -2,6 +2,9 @@ from flask import render_template, Blueprint
 from flask_security import login_required, current_user
 from web import db
 from web.models.team import Team
+from web.models.result import Result
+from sqlalchemy.sql import text
+from typing import List
 
 teams = Blueprint('teams', __name__, template_folder='templates')
 
@@ -9,12 +12,21 @@ teams = Blueprint('teams', __name__, template_folder='templates')
 def index():
     return render_template('teams/index.html', teams=Team.query.all())
 
+def get_results_for_show_page(team_id: int) -> List[Result]:
+    return db.session.query(Result).from_statement(
+        text("""WITH results AS (
+            SELECT r.*, ROW_NUMBER() OVER (PARTITION BY attack_id ORDER BY created_at DESC) AS rn
+            FROM result as r
+            ) SELECT * from results WHERE rn = 1 AND team_id = :team_id;
+            """)
+    ).params(team_id=team_id).all()
+
 @teams.route('/<int:team_id>')
 def show(team_id):
     team=Team.query.get_or_404(team_id)
-    return render_template('teams/show.html', team=team)
+    return render_template('teams/show.html', team=team, results=get_results_for_show_page(team_id))
 
 @teams.route('/me')
 @login_required
 def me():
-    return render_template('teams/show.html', team=current_user.team)
+    return show(current_user.team_id)
