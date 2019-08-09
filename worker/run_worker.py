6ac8342232c255e7fd9128d3974972a4f906fb29
Chandler Swift <chandler@chandlerswift.com>
2019-08-09 10:03:21 -0500

    Remove multiprocessing from worker

--- worker/run_worker.py
+++ worker/run_worker.py
@@ -2,8 +2,13 @@ import logging
 import sys
 from redis import Redis
 import os
+from time import sleep
+from datetime import datetime
 
-from worker import Scorer, ScoringConfig
+from attack import Attack
+from result import Result, session
+from worker import Exerciser, Gold
+from utils import are_dirs_same
 
 sys.path.append('/pack')
 import config
@@ -14,8 +19,55 @@ if __name__ == '__main__':
                    db=os.environ.get('REDIS_DB', 0))
     logging.basicConfig(level=logging.INFO)
 
-    scorer_config = ScoringConfig(config.SCORING_BIN_NAME, config.SCORING_GOLD_NAME, "/pack/gold")
+    while True:
+        (queue, task, priority) = redis.bzpopmin('tasks')
+        task = task.decode()
+        if task is None:
+            sleep(0.1)
+            continue
+        logging.getLogger(__name__).log.info(f'Got task: {task}')
+        (team_id, attack_id) = task.split('-')
+        score(team_id, attack_id)
 
-    s = Scorer(scorer_config, redis)
-    s.start()
-    s.join()
+def score(self, team_id: int, attack_id: int):
+    attack_path = os.path.join('/cctf/attacks/', str(attack_id))
+    with Exerciser(config.SCORING_BIN_NAME,
+                    git_remote=os.path.join('/cctf/repos', str(team_id)),
+                    attack=Attack(attack_path)) as team_exerciser, \
+            Gold(config.SCORING_GOLD_NAME,
+                    gold_src="/pack/gold",
+                    attack=Attack(attack_path)) as gold:
+        start_time = datetime.now()
+
+        team_result = team_exerciser.run()
+        gold_result = gold.run()
+
+        passed = True
+        output = ""
+        if True:# self.config.score_stdout:
+            if team_result.stdout != gold_result.stdout:
+                passed = False
+                output += f"STDOUT: expected: {gold_result.stdout}, received: {team_result.stdout}\n"
+        if True:# self.config.score_stderr:
+            if team_result.stderr != gold_result.stderr:
+                passed = False
+                output += f"STDERR: expected: {gold_result.stderr}, received: {team_result.stderr}\n"
+        if True:# self.config.score_exit_code:
+            if team_result.exit_code != gold_result.exit_code:
+                passed = False
+                output += f"EXIT CODE: expected: {gold_result.exit_code}, received: {team_result.exit_code}\n"
+        if False:# self.config.score_working_dir:
+            if not are_dirs_same(team_result.directory, gold_result.directory):
+                passed = False
+                output += f"WORKING DIR: Files were different.\n"
+
+        result = Result()
+        result.attack_id = attack_id
+        result.team_id = team_id
+        result.commit_hash = team_result.commit_checksum
+        result.passed = passed
+        result.output = output
+        result.start_time = start_time
+        result.seconds_to_complete = team_result.time_sec + gold_result.time_sec
+        session.add(result)
+        session.commit()
