711048c60e351cc932e0e3d55f6b953f8e452cd3
Chandler Swift <chandler@chandlerswift.com>
2021-01-26 22:48:39 -0600

    Clean up create test page, start integrating existing attacks

--- web/web/blueprints/test/__init__.py
+++ web/web/blueprints/test/__init__.py
@@ -1,14 +1,25 @@
-from os import F_TEST
 from flask import render_template, Blueprint, make_response, request, abort, flash, redirect, Response
 from flask_security import login_required, current_user
-from web import db, redis, team_required
+from web import db, redis, team_required, formatters
 
 test = Blueprint('test', __name__, template_folder='templates')
 
 @test.route('/')
 @login_required
 @team_required
-def index():
-    return render_template('test/index.html')
+def index(): # TODO
+    return render_template('test/index.html',
+            passing_results=current_user.team.passing,
+            failing_results=current_user.team.failing,
+            formatters=formatters,
+    )
 
-
+@test.route('/new')
+@login_required
+@team_required
+def create():
+    return render_template('test/index.html',
+            passing_results=current_user.team.passing,
+            failing_results=current_user.team.failing,
+            formatters=formatters,
+    )
