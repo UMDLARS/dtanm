1743e9c499ca8c9b58f8a9bb1ad13d9cd86cec89
Chandler Swift <chandler@chandlerswift.com>
2021-01-27 19:44:17 -0600

    Rename test blueprint to tests
    This lines up much better with the format we have with the other blueprints.

--- web/web/blueprints/tests/__init__.py
+++ web/web/blueprints/tests/__init__.py
@@ -2,23 +2,23 @@ from flask import render_template, Blueprint, make_response, request, abort, fla
 from flask_security import login_required, current_user
 from web import db, redis, team_required, formatters
 
-test = Blueprint('test', __name__, template_folder='templates')
+tests = Blueprint('tests', __name__, template_folder='templates')
 
-@test.route('/')
+@tests.route('/')
 @login_required
 @team_required
 def index(): # TODO
-    return render_template('test/index.html',
+    return render_template('tests/index.html',
             passing_results=current_user.team.passing,
             failing_results=current_user.team.failing,
             formatters=formatters,
     )
 
-@test.route('/new')
+@tests.route('/new')
 @login_required
 @team_required
 def create():
-    return render_template('test/index.html',
+    return render_template('tests/index.html',
             passing_results=current_user.team.passing,
             failing_results=current_user.team.failing,
             formatters=formatters,
