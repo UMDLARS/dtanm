8813ba52e25318dc98a56e88949758c391414e96
Chandler Swift <chandler@chandlerswift.com>
2021-02-02 20:36:48 -0600

    Add more basic tests

--- integration_tests/test_basic.py
+++ integration_tests/test_basic.py
@@ -1,7 +1,33 @@
 import requests
 
 
-def test_status_code(http_service):
+def test_load_home_page(http_service):
     response = requests.get(http_service)
-
     assert response.status_code == 200
+    assert "Welcome to DTANM!" in response.content.decode()
+
+def test_load_instructions(http_service):
+    response = requests.get(f"{http_service}/instructions/")
+    assert response.status_code == 200
+    assert "The sloths and the eagles are at all-out war." in response.content.decode()
+
+    response = requests.get(f"{http_service}/instructions/examples")
+    assert response.status_code == 200
+    assert "EXAMPLE TEST CASES" in response.content.decode()
+
+def test_stats_ui(http_service):
+    response = requests.get(f"{http_service}/stats")
+    assert response.status_code == 200
+    assert "Competition Statistics" in response.content.decode()
+
+def test_stats_json(http_service):
+    response = requests.get(f"{http_service}/stats.json")
+    assert response.status_code == 200
+    assert "Attacks submitted" in response.json()
+
+def test_functional_scoring_worker(http_service):
+    response = requests.get(f"{http_service}/stats.json")
+    assert response.status_code == 200
+    assert "Scoring workers" in response.json()
+    assert response.json()["Scoring workers"] > 0
+
