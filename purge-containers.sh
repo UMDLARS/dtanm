4e8b1f99f27a97366930a34961fc7e880c6a2254
Zachary Kirtz <zach.kirtz@gmail.com>
2026-05-21 18:46:39 -0500

    remove my pruge scripts

--- purge-containers.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CONT="$(docker container ls -a | tail -n +2)"
-
-while IFS= read -r line; do
-    docker container rm $(echo "$line" | head -c 12)
-done <<< "$CONT"
--- purge-images.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CONT="$(docker image ls -a | tail -n +2)"
-
-while IFS= read -r line; do
-    docker image rm $(echo "$line" | cut -d ' ' -f 1)
-done <<< "$CONT"
